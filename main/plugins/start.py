0b0e3eaba71b7a0165e703f4cd27270f375d3823
Shashank Sharma <78837537+theshashankk@users.noreply.github.com>
2023-05-02 20:13:28 +0530

    Update start.py

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -8,16 +8,11 @@ async def strt(event):
     user_name = event.sender.username 
     if event.sender_id != AUTH_USER:
         buttons = [
-            [Button.inline("Redirect ⬇️", data='wigensjs')],
-            [Button.url(name, url=f"t.me/{user_name}")]
+            [Button.inline("1 weeks", data="oneweek"), Button.url("Purchase now", "t.me/xdcomrade")],
+            [Button.inline("2 weeks", data="twoweek"), Button.url("Purchase now", "t.me/xdcomrade")],
+            [Button.inline("3 weeks", data="threeweeks"), Button.url("Purchase now", "t.me/xdcomrade")],
+            [Button.inline("1 months", data="onemonths"), Button.url("Purchase now", "t.me/xdcomrade")]
         ]
-        await bot.send_file(user_id, file='https://telegra.ph/file/1f72ad102d6194d02bb13.jpg', caption='**Hey there**\n\n__Contact @xD_Comrade to purchase subscription of AD Bot__\n\n**Have a nice day**')
-        await bot(functions.messages.SendInlineBotResultRequest(
-            user_id,
-            query_id='bot-started',
-            result_id='some-result-id',
-            message=f'**New User Started the bot**\n\nUser id: {user_id}\nUser name: {user_name}',
-            reply_markup=types.InlineKeyboardMarkup(buttons)
-        ))
-    elif event.sender_id == AUTH_USER:
-        await bot.send_file(user_id, file='https://telegra.ph/file/1f72ad102d6194d02bb13.jpg', caption='**Hello master!**\n\n__Use Below buttons to go through my menu__\n\n**Have a nice day**')
+        await bot.send_message(event.sender_id, "__Hey there__\n\nI'm ad bot, i can do promotion of your content, products aything you want to promote.\n\n**Use the below buttons to get subscription information ", buttons=buttons)
+    else:
+        await bot.send_message(event.sender_id, 'hello master')
