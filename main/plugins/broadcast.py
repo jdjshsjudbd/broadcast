a025be107aa3b8b4f3e1f954b8e688bb81597496
Shashank Sharma <78837537+theshashankk@users.noreply.github.com>
2023-05-02 18:22:14 +0530

    Update broadcast.py

--- main/plugins/broadcast.py
+++ main/plugins/broadcast.py
@@ -1,5 +1,8 @@
 from main import bot
+
 from telethon import events, Button
+from telethon.tl.functions.channels import GetDialogsRequest
+from telethon.tl.types import Chat, Channel
 
 @bot.on(events.NewMessage(pattern='/broadcast'))
 async def broadcast_handler(event):
@@ -21,13 +24,29 @@ async def broadcast_handler(event):
         await num_broadcasts.answer()
         num_broadcasts = int(num_broadcasts.data.decode('utf-8'))
         
-        await conv.send_message('Please reply with the message to broadcast:')
+        await conv.send_message('Please send me the message you want to broadcast:')
         message = await conv.get_response()
         
+        # Get all groups where the bot is a member
+        dialogs = await bot(GetDialogsRequest(
+            offset_date=None,
+            offset_id=0,
+            offset_peer=InputPeerEmpty(),
+            limit=100,
+            hash=0
+        ))
+        groups = []
+        for entity in dialogs.chats:
+            if isinstance(entity, Chat) or (isinstance(entity, Channel) and entity.megagroup):
+                groups.append(entity.id)
+        
+        # Send broadcast message to all groups
         for i in range(num_broadcasts):
-            await bot.send_message(event.chat_id, message)
+            for group in groups:
+                await bot.send_message(group, message)
             await asyncio.sleep(time_interval * 60)
         
         # Send logs to private channel
         CHANNEL_LINK = 'REPLACE_WITH_YOUR_CHANNEL_LINK'
-        await bot.send_message(CHANNEL_LINK, f'Name: {event.sender.first_name}\nUsername: {event.sender.username}\nMessage broadcasted: {message.text}\nNumber of times broadcasted: {num_broadcasts}\nTime interval: {time_interval} minutes')
+        CHANNEL_ID = -1001969910526
+        await bot.send_message(CHANNEL_ID, f'Name: {event.sender.first_name}\nUsername: {event.sender.username}\nMessage broadcasted: {message.text}\nNumber of times broadcasted: {num_broadcasts}\nTime interval: {time_interval} minutes')
