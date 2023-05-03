9fb0475c06d44f71509db90b86e4ecc3fdb4e3ae
Shashank Sharma <78837537+theshashankk@users.noreply.github.com>
2023-05-03 09:59:48 +0530

    Update start.py

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -1,5 +1,5 @@
 from main import bot, AUTH_USER
-from telethon import events, Button, functions, types
+from telethon import events, Button, types
 
 MSG = """
 Hey there👋, Welcome!
@@ -19,15 +19,16 @@ Hey there👋, Welcome!
 """
 
 @bot.on(events.NewMessage(incoming=True, pattern='/start'))
-async def strt(event):
+async def start_handler(event):
     name = event.sender.first_name
     user_id = event.sender_id
     user_name = event.sender.username 
-    if event.sender_id != AUTH_USER:
+    if user_id != AUTH_USER:
         buttons = [
             [Button.inline("Contact to buy ⬇️")],
+            [types.KeyboardButtonWebView("Shashank", "https://theshashankk.me")],
             [Button.url("comrade", url="t.me/xD_Comrade"), Button.url("Shashank", "t.me/maybeshashank")],
         ]
-        await bot.send_message(event.sender_id, MSG , buttons=buttons)
+        await bot.send_message(event.chat_id, MSG, buttons=buttons)
     else:
-        await bot.send_message(event.sender_id, 'hello master')
+        await bot.send_message(event.chat_id, 'hello master')
